89d0550d6bf3c5b6918ab2714c28dfd786dc580f
Hanish Madasu <82875504+hanish445@users.noreply.github.com>
2025-05-05 14:24:10 +0200

    Issue 3

---
+++ server/controllers/authController.js
@@ -0,0 +1,33 @@
+const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+const { generateToken } = require('../config/jwtConfig');
+
+exports.register = async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) return res.status(400).json({ message: 'User already exists' });
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({ username, email, password: hashedPassword });
+        await user.save();
+        const token = generateToken(user);
+        res.status(201).json({ token });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user || !await bcrypt.compare(password, user.password)) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+        const token = generateToken(user);
+        res.json({ token });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+};
