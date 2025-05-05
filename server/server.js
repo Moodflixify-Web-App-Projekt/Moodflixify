ae7b4a4211042451b8c502b13dc8597c6e492a93
Hanish Madasu <82875504+hanish445@users.noreply.github.com>
2025-05-05 09:46:41 +0200

    Reset for new code

--- server/server.js
+++ server/server.js
@@ -1,42 +1,11 @@
-//const express = require('express');
-//const app = express();
-//const PORT = process.env.PORT || 5000;
-
-//app.get('/', (req, res) => {
-    //res.send("Hello from the backend!");
-//});
-
-//app.listen(PORT, () => {
-    //console.log(`Server is running on port: ${PORT}`);
-//});
-
-const express = require("express");
-const connectDB = require("./config/db");
-require('dotenv').config({ path: "./.env"});
-
+const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.send("Moodflixify Server is running!");
+app.get('/', (req, res) => {
+    res.send("Hello from the backend!");
 });
 
-async function startServer(){
-    try{
-        await connectDB();
-        const PORT = process.env.PORT || 5000;
-        app.listen(PORT, () => {
-            console.log(`Server started on port ${PORT}`);
-        });
-    }
-    catch(error){
-        console.error("Server failed to start", error);
-        process.exit(1);
-    }
-}
-
-startServer().catch(error => {
-    console.error("Failed to start server", error);
-    process.exit(1);
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
 });
