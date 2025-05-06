67aeff6bf2d8bc8dea6774527474a42666a0de00
Sarah <slanger06@gmail.com>
2025-05-06 15:16:31 +0200

    Issue 7

---
+++ server/scripts/seedDB.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const User = require('../models/userModel');
+const connectDB = require('../config/db');
+
+const seedDB = async () => {
+
+    try {
+        await connectDB();
+        await User.deleteMany({});
+
+        const testUser = new User({
+            username: 'testuser',
+            email: 'testuser@example.com',
+            password: 'password123',
+        });
+
+        await testUser.save();
+
+        console.log('Database connected');
+        process.exit(0);
+    }
+    catch(error){
+        console.error('Error while trying to connect to MongoDB', error);
+        process.exit(1);
+    }
+
+};
+
+seedDB();
