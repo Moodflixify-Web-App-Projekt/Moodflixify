620ed95ace0af79d9acfa6e3f32e092feb433a1f
Hanish Madasu <82875504+hanish445@users.noreply.github.com>
2025-05-20 16:34:02 +0200

    API calls repair

--- server/routes/moodRoutes.js
+++ server/routes/moodRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {getRecommendations, addToWatchlist, getWatchlist} = require('../controllers/moodController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-router.get('recommendations', authMiddleware, getRecommendations);
+router.get('/recommendations', authMiddleware, getRecommendations);
 router.post('/watchlist', authMiddleware, addToWatchlist);
 router.get('/watchlist', authMiddleware, getWatchlist);
 
