4d20c55e07d6e2bb3c86487a1fd3df0543c3bc1d
Hanish Madasu <82875504+hanish445@users.noreply.github.com>
2025-06-08 15:11:51 +0200

    Issue 24

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_BACKEND_API_URL || 'http://localhost:5000/api';
-
 const api = axios.create({
-    baseURL: API_BASE_URL,
+    baseURL: 'http://localhost:5000/api',
 });
 
 api.interceptors.request.use((config) => {
