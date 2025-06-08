8313107456b93f42a349b065227fa287d622417b
Hanish Madasu <82875504+hanish445@users.noreply.github.com>
2025-06-08 18:18:41 +0200

    Issue 24

--- server/server.js
+++ server/server.js
@@ -16,11 +16,7 @@ connectDB().then(() => {
 });
 
 // Middleware
-app.use(cors({
-    origin: 'https://moodflixify.netlify.app/',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders:['Content-type', 'Authorization']
-}));
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
