a5be85c6b3d675e9930570751dc7327f2fd48bca
Hanish Madasu <82875504+hanish445@users.noreply.github.com>
2025-06-08 17:06:49 +0200

    Issue 25

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ connectDB().then(() => {
 
 // Middleware
 app.use(cors({
-    origin: 'https://moodflixify.netlify.app',
+    origin: 'https://moodflixify.netlify.app/' || 'http://localhost:5173/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders:['Content-type', 'Authorization']
 }));
